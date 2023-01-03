5f71ef046c3e5a49fd273a0e20a855ef2590458d
t-kuraga <agomez.software@gmail.com>
2023-01-03 13:41:34 -0500

    Added Optimal expenses logic

--- index.js
+++ index.js
@@ -1,16 +1,152 @@
-import { BidCalculator } from './js/BidCalculator.js';
+import { BidCalculator, getTotalSpent } from './js/BidCalculator.js';
 import testData from './data/tests.json';
 
 document.getElementById('calculateBtn').addEventListener('click', (e) => {
-  const iterations = document.getElementById('iterations').value;
-  const decimals = document.getElementById('decimals').value;
+  // Get values from page
+  const iterations = parseInt(document.getElementById('iterations').value);
+  const decimals = document.getElementById('precisiondecimals').value;
+  const precision = parseFloat(`1e-${decimals}`);
+  const moneyDecimals = parseInt(
+    document.getElementById('moneydecimals').value
+  );
+  const moneyPrecision = Math.pow(10, moneyDecimals);
 
+  // Get the optimal expenses
   console.log(
     `Starting calculations with ${iterations} iterations and ${decimals} decimals of precision...`
   );
-  for (let i = 0; i < testData.length; i++) {
-    const calculator = new BidCalculator(testData[i].budget);
-    var x = calculator.calculateBid(x);
-    console.log(x);
-  }
+  const results = getResults(iterations, precision, moneyPrecision);
+  console.log(results);
+
+  // Create a table and present the results
+  createTable('test_results', testData, results, moneyPrecision);
 });
+
+/**
+ * Calculate results for all test scenarios
+ * @param {Number} iterations Max iterations to search
+ * @param {Number} precision Decimal precision (1x10^n)
+ * @param {Number} moneyPrecision Decimals for currency calculations (10^n)
+ * @returns Array with optimal expenses per test case scenario
+ */
+const getResults = (iterations, precision, moneyPrecision) => {
+  const results = [];
+  for (let i = 0; i < testData.length; i++)
+    results.push(
+      new BidCalculator(testData[i].budget, moneyPrecision).calculate(
+        iterations,
+        precision
+      )
+    );
+
+  return results;
+};
+
+/**
+ * Creates a result table for test results
+ * @param {string} tableId Table id
+ * @param {Array} testData Target test data
+ * @param {Array} results Test results
+ * @param {Number} moneyPrecision Decimals for currency calculations (10^n)
+ */
+const createTable = (tableId, testData, results, moneyPrecision) => {
+  // Map all the data into a single data set
+  const data = testData.map((t, i) => ({
+    budget: t.budget,
+    expectedBid: t.expected.bid,
+    expectedBasicFee: t.expected.basicFee,
+    expectedSpecialFee: t.expected.specialFee,
+    expectedAssociationFee: t.expected.associationFee,
+    expectedStorageFee: t.expected.storageFee,
+    actualBid: results[i].bid,
+    actualBasicFee: results[i].basicFee,
+    actualSpecialFee: results[i].specialFee,
+    actualAssociationFee: results[i].associationFee,
+    actualStorageFee: results[i].storageFee,
+    match: assertEqual(t.expected, results[i], moneyPrecision),
+    expectedSum: getTotalSpent(t.expected),
+    actualSum: getTotalSpent(results[i]),
+  }));
+
+  // Create the table
+  const table = $('#' + tableId);
+  table.bootstrapTable?.('destroy'); // Destroy the table if already created
+  table.bootstrapTable({
+    data,
+    pagination: true,
+    columns: [
+      {
+        field: 'budget',
+        title: 'Budget',
+      },
+      {
+        field: 'expectedBid',
+        title: 'Expected Bid',
+      },
+      {
+        field: 'actualBid',
+        title: 'Actual Bid',
+      },
+      {
+        field: 'expectedBasicFee',
+        title: 'Expected Basic Fee',
+      },
+      {
+        field: 'actualBasicFee',
+        title: 'Actual Basic Fee',
+      },
+      {
+        field: 'expectedSpecialFee',
+        title: 'Expected Special Fee',
+      },
+      {
+        field: 'actualSpecialFee',
+        title: 'Actual Special Fee',
+      },
+      {
+        field: 'expectedAssociationFee',
+        title: 'Expected Association Fee',
+      },
+      {
+        field: 'actualAssociationFee',
+        title: 'Actual Association Fee',
+      },
+      {
+        field: 'expectedStorageFee',
+        title: 'Expected Storage Fee',
+      },
+      {
+        field: 'actualStorageFee',
+        title: 'Actual Storage Fee',
+      },
+      {
+        field: 'match',
+        title: 'Match (Result rounded to 2 decimals)',
+      },
+      {
+        field: 'expectedSum',
+        title: 'Expected Total Spent',
+      },
+      {
+        field: 'actualSum',
+        title: 'Actual Total Spent',
+      },
+    ],
+  });
+};
+
+/**
+ * Assert whether a set of expenses comply with their expected values
+ * @param {Object} expected
+ * @param {Object} actual
+ * @param {Number} moneyPrecision Decimals for currency calculations (10^n)
+ * @returns Whether a set of expenses comply with their expected values
+ */
+const assertEqual = (expected, actual, moneyPrecision) =>
+  Object.keys(expected).reduce(
+    (a, k) =>
+      a &&
+      parseFloat(expected[k]) ===
+        Math.round(actual[k] * moneyPrecision) / moneyPrecision,
+    true
+  );
--- js/BidCalculator.js
+++ js/BidCalculator.js
@@ -1,56 +1,169 @@
+/**
+ * Bid Calculator class
+ */
 export class BidCalculator {
-  budget;
+  _budget;
+  _moneyPrecision;
 
-  _bid = 0;
-  get bid() {
-    return this._bid;
-  }
-  _basicFee = 0;
-  get basicFee() {
-    return this._basicFee;
-  }
-  _specialFee = 0;
-  get specialFee() {
-    return this._specialFee;
-  }
-  _associationFee = 0;
-  get associationFee() {
-    return this._associationFee;
-  }
-  _storageFee = 100;
-  get storageFee() {
-    return this._storageFee;
+  /**
+   * Constructor
+   * @param {Number} budget Target budget
+   * @param {Number} moneyPrecision Decimals for currency calculations (10^n)
+   */
+  constructor(budget, moneyPrecision) {
+    this._budget = budget;
+    this._moneyPrecision = moneyPrecision;
   }
 
-  constructor(budget) {
-    this.budget = budget;
-  }
-
-  _setBasicFee(x) {
+  /**
+   * Calculate the basic fee for a given bid
+   * @param {Number} x Target bid
+   * @returns Fee
+   */
+  _getBasicFee(x) {
     const percentage = x * 0.1;
-    this._basicFee = percentage <= 10 ? 10 : percentage >= 50 ? 50 : percentage;
+    return percentage <= 10 ? 10 : percentage < 50 ? percentage : 50;
   }
 
-  _setSpecialFee(x) {
-    this._specialFee = x * 0.02;
+  /**
+   * Calculate the special fee for a given bid
+   * @param {Number} x Target bid
+   * @returns Fee
+   */
+  _getSpecialFee(x) {
+    return Math.round(x * 0.02 * this._moneyPrecision) / this._moneyPrecision;
   }
 
-  _setAssociationFee(x) {
-    this._associationFee =
-      x < 1 ? 0 : x >= 1 && x <= 500 ? 5 : x <= 1000 ? 10 : x <= 3000 ? 15 : 20;
+  /**
+   * Calculate the association fee for a given bid
+   * @param {Number} x Target bid
+   * @returns Fee
+   */
+  _getAssociationFee(x) {
+    return x < 1
+      ? 0
+      : x >= 1 && x <= 500
+      ? 5
+      : x <= 1000
+      ? 10
+      : x <= 3000
+      ? 15
+      : 20;
   }
 
-  calculateBid(x) {
-    this._setBasicFee(x);
-    this._setSpecialFee(x);
-    this._setAssociationFee(x);
-    this._bid =
-      x +
-      this._basicFee +
-      this._specialFee +
-      this._storageFee +
-      this._associationFee -
-      this.budget;
-    return this._bid;
+  /**
+   * Calculates the total expenses for a given bid
+   * @param {Number} x Target bid
+   * @returns Object with calculated expenses
+   */
+  calculateExpenses(x) {
+    return {
+      bid: x,
+      basicFee: this._getBasicFee(x),
+      specialFee: this._getSpecialFee(x),
+      associationFee: this._getAssociationFee(x),
+      storageFee: 100,
+    };
   }
+
+  /**
+   * Numerical method to find the optimal bid
+   * @param {Number} iterations Max iterations to search
+   * @param {Number} precision Decimal precision (1x10^n)
+   * @returns Optimal bid with calculated expenses
+   */
+  calculate(iterations, precision) {
+    try {
+      // A numerical method should do a binary search on the error function for
+      // the problem:
+      // I use a binary method instead of, say, regula falsi, to force boundaries on both sides of
+      // the error function to come close to it, in case the closest approach has a negative error.
+      // A negative error means you spend more than the budget, even if it is a decimal part.
+      const [x, error] = bisect(
+        // This is the error function: The difference between the total spent and the budget
+        (x) => this._budget - getTotalSpent(this.calculateExpenses(x)),
+        iterations,
+        precision,
+        this._moneyPrecision,
+        precision,
+        this._budget
+      );
+
+      console.log(x);
+      return this.calculateExpenses(x);
+    } catch (error) {
+      console.error(`No solutions where found for Budget ${this.budget}`);
+      return {
+        bid: 0,
+        basicFee: 0,
+        specialFee: 0,
+        associationFee: 0,
+        storageFee: 0,
+      };
+    }
+  }
+}
+
+/**
+ * Adds up all expenses to get a total
+ * @param {Object} expenses Expenses object
+ * @returns Total spent
+ */
+export function getTotalSpent(expenses) {
+  return (
+    expenses.bid +
+    expenses.basicFee +
+    expenses.specialFee +
+    expenses.storageFee +
+    expenses.associationFee
+  );
+}
+
+/**
+ * A numerical bisection method to perform a binary search on continuous functions inside an interval
+ * @see https://en.wikipedia.org/wiki/Bisection_method
+ * @param {Function} func Target function
+ * @param {Number} iterations Max iterations to search
+ * @param {Number} precision Decimal precision (1x10^n)
+ * @param {Number} xPrecision Precision for x axis values (10^n)
+ * @param {Number} a Interval start
+ * @param {Number} b Interval end
+ * @returns Closest x value to a root, and error for x value
+ */
+function bisect(func, iterations, precision, xPrecision, a, b) {
+  let error_a = func(a);
+  let error_b = func(b);
+  if (error_a * error_b >= 0) throw 'You have not assumed right a and b\n';
+
+  // Initialize result
+  let c;
+  let error_c;
+
+  for (let i = 0; i < iterations; i++) {
+    // Find the point that touches x axis
+    c = Math.round(((a + b) / 2) * xPrecision) / xPrecision;
+    error_c = func(c);
+
+    // Check if the above found point is root
+    if (c === a || c === b || error_c == 0 || Math.abs(error_c) <= precision)
+      break;
+
+    // Decide the side to repeat the steps
+    if (error_c * error_a < 0) {
+      b = c;
+      error_b = error_c;
+    } else {
+      a = c;
+      error_a = error_c;
+    }
+  }
+
+  // Although a root with a negative error could be more precise, the required problem
+  // mandates not to over spend. Therefore, the x value with the smallest positive error
+  // is to be returned
+  return error_c >= 0
+    ? [c, error_c]
+    : error_a !== error_c && error_a >= 0
+    ? [a, error_a]
+    : [b, error_b];
 }
